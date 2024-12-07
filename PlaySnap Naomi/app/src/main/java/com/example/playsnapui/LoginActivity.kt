2a8de108ca2e9c1c21a2a4ac5f3917c642a81141
Teresa Naomi <teresanaomi10@gmail.com>
2024-12-07 20:22:46 +0700

    Main Page, Login, Register UI - Revisi

--- PlaySnap Naomi/app/src/main/java/com/example/playsnapui/LoginActivity.kt
+++ PlaySnap Naomi/app/src/main/java/com/example/playsnapui/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.example.playsnapui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.ContactsContract.CommonDataKinds.Email
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
@@ -11,10 +13,11 @@ import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
 
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+
 
     }
 }
--- PlaySnap Naomi/app/src/main/java/com/example/playsnapui/MainActivity.kt
+++ PlaySnap Naomi/app/src/main/java/com/example/playsnapui/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.example.playsnapui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.widget.AppCompatButton
@@ -9,11 +11,27 @@ import org.w3c.dom.Text
 import java.text.DateFormat
 
 class MainActivity : AppCompatActivity() {
+    lateinit var button1 : Button
+    lateinit var button2 : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        button1 = findViewById(R.id.btn_login)
+        button2 = findViewById(R.id.btn_signup)
+
+        button1.setOnClickListener{
+            val explicit = Intent(this, LoginActivity::class.java)
+
+            startActivity(explicit)
+        }
+
+        button2.setOnClickListener{
+            val explicit = Intent(this, RegisterActivity::class.java)
+
+            startActivity(explicit)
+        }
 
     }
 }
