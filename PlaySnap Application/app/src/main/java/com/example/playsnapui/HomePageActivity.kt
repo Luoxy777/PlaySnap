8b6e75610048a8648d3596cae210992b15277c90
Luoxy77 <dianeluoxyzabdi@gmail.com>
2025-02-04 00:46:40 +0700

    Camera Functionality

--- PlaySnap Application/app/src/main/java/com/example/playsnapui/HomePageActivity.kt
+++ PlaySnap Application/app/src/main/java/com/example/playsnapui/HomePageActivity.kt
@@ -1,11 +1,23 @@
 package com.example.playsnapui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.playsnapui.databinding.ActivityHomePageBinding
 
 class HomePageActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityHomePageBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home_page)
+        binding = ActivityHomePageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnScanObject.setOnClickListener{
+            val explicit = Intent(this, SearchByCameraActivity::class.java)
+
+            startActivity(explicit)
+        }
     }
 }
