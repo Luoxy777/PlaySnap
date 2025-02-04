bef56056315a65792c6a40128d8d1c6dbcad565b
Teresa Naomi <teresanaomi10@gmail.com>
2025-02-04 15:00:26 +0700

    Reset

--- PlaySnap Application/app/src/main/java/com/example/playsnapui/HomePageActivity.kt
+++ PlaySnap Application/app/src/main/java/com/example/playsnapui/HomePageActivity.kt
@@ -1,23 +1,11 @@
 package com.example.playsnapui
 
-import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.example.playsnapui.databinding.ActivityHomePageBinding
 
 class HomePageActivity : AppCompatActivity() {
-
-    lateinit var binding: ActivityHomePageBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityHomePageBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-
-        binding.btnScanObject.setOnClickListener{
-            val explicit = Intent(this, SearchByCameraActivity::class.java)
-
-            startActivity(explicit)
-        }
+        setContentView(R.layout.activity_home_page)
     }
 }
